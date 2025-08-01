3732eda6c7e5097badb9fd32bebdaef4bc0153b5
Aeiddius <adriantagayom@gmail.com>
2025-08-01 16:01:27 +0800

    fixed. working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
 fn main() {
-    let ex: String = String::from("4 / 2 + ( 7 * 3 )");
+    let ex: String = String::from("( ( 9 + 3 ) * 2 - 6 )");
 
 
 
     let postfix: String = convert_to_postfix(&ex);
-    let evaluated_rpn: u32 = evaluate_rpn(&postfix);
+    let evaluated_rpn: f64 = evaluate_rpn(&postfix);
     println!("{postfix}");
     println!("Final result: {evaluated_rpn}");
 }
@@ -26,27 +26,45 @@ fn convert_to_postfix(infix_expression: &String) -> String {
 
     let tokens: Vec<&str> = infix_expression.split_whitespace().collect();
     for token in tokens {
+        
         match token.trim().parse::<u32>() {
             Ok(_n) => {
                 output.push(token);
+                println!("Pushing to output: {token} | {output:?}")
             },
             Err(_err) => {
                 
                 if token == "(" {
                     stack.push(token);
-                    println!("hello world");
+                    println!("Pushing to stack sub: {token} | {stack:?}")
                 } else if token == ")" {
                     while !stack.is_empty() && stack.last().unwrap() != &"(" {
                         output.push(stack.pop().unwrap());
+                        println!("Pushing to output sub: {token} | {output:?} |           | {stack:?}")
                     }
                     stack.pop(); // pop last parenthesis
-                    continue;
+                    println!("Pushing to output sub popped: {token} | {stack:?}")
                 } else {
-                    while !stack.is_empty() && get_priority(&priorities, stack.last().unwrap()) >= get_priority( &priorities,&token) {
-                        output.push(stack.pop().unwrap());
+                    if !stack.is_empty() && stack.last().unwrap() == &"(" {
+                        stack.push(token);
+                        println!("Pushing to stack: {token} | {stack:?}");
+                    } else {
+                        loop {
+                            let last = stack.last().unwrap();
+                            if stack.is_empty() || last == &"(" {
+                                break;
+                            }
+                            if get_priority(&priorities, last) >= get_priority(&priorities, &token) {
+                            output.push(stack.pop().unwrap());
+                            println!("Pushing to ouput the stack pop: {token} | {output:?}");
+                            }
+                        }
+                        stack.push(token);
+                        println!("Pushing to stack: {token} | {stack:?}");
+
                     }
-                    // println!("{token}");
-                    stack.push(token);
+
+      
                 }
             }
         }
@@ -56,8 +74,7 @@ fn convert_to_postfix(infix_expression: &String) -> String {
         output.push(stack.pop().unwrap());
     }
 
-    println!("{output:?}");
-    println!("{stack:?}");
+
     output
         .iter()
         .map(|n| n.to_string())
@@ -71,20 +88,20 @@ fn get_priority<'a>(operators: &'a HashMap<&'a str, u8>, key: &'a str) -> &'a u8
 }
 
 // Reverse Polish Notation
-fn evaluate_rpn(expression: &String) -> u32 {
+fn evaluate_rpn(expression: &String) -> f64 {
     let list: Vec<&str> = expression.split_whitespace().collect();
     
-    let mut stack: Vec<u32> = Vec::new();
+    let mut stack: Vec<f64> = Vec::new();
 
     for s in list {
-        match s.trim().parse::<u32>() {
+        match s.trim().parse::<f64>() {
             Ok(n) => {
                 stack.push(n);
                 println!("Num: {}", n);
             },
             Err(_err) => {
-                let b = stack.pop().expect("there should be two");
-                let a = stack.pop().expect("there should be two");
+                let b: f64 = stack.pop().expect("there should be two");
+                let a: f64 = stack.pop().expect("there should be two");
 
                 println!("Ope: {}", s);
                 match s {
@@ -92,7 +109,7 @@ fn evaluate_rpn(expression: &String) -> u32 {
                     "-" => {println!("Sub {a} - {b}");stack.push(a - b);},
                     "*" => {println!("Mul {a} * {b}");stack.push(a * b);},
                     "/" => {println!("Div {a} / {b}");stack.push(a / b);},
-                    "^" => {println!("Pow {a} ^ {b}");stack.push(a.pow(b));},
+                    "^" => {println!("Pow {a} ^ {b}");stack.push(a.powf(b));},
                     _ => {}
                 }
             }
